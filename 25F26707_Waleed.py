e6ec7a011867b9d9fdbfd9c8d3bfcc7c9c7b75f5
Waleed <145193257+shikashd@users.noreply.github.com>
2026-09-02 14:41:00 +0400

    Modify high-value classification message

--- 25F26707_Waleed.py
+++ 25F26707_Waleed.py
@@ -2,7 +2,7 @@ customer_spending = [1200, 750, 300, 1500, 450]
 
 for spending in customer_spending:
     if spending >= 1000:
-        print(spending, "- High-value customer")
+        print(spending, "- VIP High-value customer")
     elif spending >= 500:
         print(spending, "- Medium-value customer")
     else:
