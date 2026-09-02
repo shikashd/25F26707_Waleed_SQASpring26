120a300e15358bc366286dccf1a2abfc396d574b
Waleed <145193257+shikashd@users.noreply.github.com>
2026-09-02 14:37:31 +0400

    Add processing message inside for loop

--- 25F26707_Waleed.py
+++ 25F26707_Waleed.py
@@ -1,6 +1,8 @@
 customer_spending = [1200, 750, 300, 1500, 450]
 
 for spending in customer_spending:
+    print("Processing spending value:", spending)
+
     if spending >= 1000:
         print(spending, "- High-value customer")
     elif spending >= 500:
